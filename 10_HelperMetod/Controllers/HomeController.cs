5be27389ed38ba33fab8f25fa5d51c42f9731990
SsoftDevelep <saitemresolmaz@gmail.com>
2017-02-22 16:17:57 +0300

    Helper Metot Nasil Yazilir ,Kullanimi

---
+++ 10_HelperMetod/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _10_HelperMetod.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Homepage()
+        {
+            return View();
+        }
+
+        public ActionResult Homepage2()
+        {
+            return View();
+        }
+
+        public ActionResult Homepage3()
+        {
+            return View();
+        }
+    }
+}
